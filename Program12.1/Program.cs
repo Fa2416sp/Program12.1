92839a541ef2a1d74c16bf968dd56dc790d10e9e
alex tabota <alex-tabota@yandex.ru>
2021-02-10 18:34:14 +0300

    Добавьте файлы проекта.

---
+++ Program12.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program12._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] A = new int[7, 7];
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    if ((i % 2 == 0) || (j % 2 == 1))
+                        A[i, j] = 8;
+                    else A[i, j] = 0;
+                    Console.Write("{0,4} ", A[i, j]);
+                }
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+        }
+    }
+}
